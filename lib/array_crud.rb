16b256a4ba4767cf878fa833eba3f259ee6ec529
Jeffrey Brown <brownjs03@gmail.com>
2019-08-17 17:42:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  coheed_songs = ["The Suffering", "Blood Red Summer", "Welcome Home"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.upshit(element)
 end
 
 def remove_element_from_end_of_array(array)
